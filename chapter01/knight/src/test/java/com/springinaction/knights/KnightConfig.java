d4c770c8608d9ce19eeef3ad2d4acc73765b23e5
Winston Spencer Sonnesen <sonnesen@gmail.com>
2016-11-18 09:54:44 -0200

    Chapter 01 Tests

---
+++ chapter01/knight/src/test/java/com/springinaction/knights/KnightConfig.java
@@ -0,0 +1,26 @@
+package com.springinaction.knights;
+
+import java.io.PrintStream;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KnightConfig {
+
+	@Bean
+	public Knight knight() {
+		return new BraveKnight(quest());
+	}
+	
+	@Bean
+	public Quest quest() {
+		return new SlayDragonQuest(stream());
+	}
+	
+	@Bean
+	public PrintStream stream() {
+		return new FakePrintStream();
+	}
+
+}
---
+++ chapter01/knight/src/test/java/com/springinaction/knights/KnightJavaConfigInjectionTest.java
@@ -0,0 +1,33 @@
+package com.springinaction.knights;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = KnightConfig.class, loader = AnnotationConfigContextLoader.class)
+public class KnightJavaConfigInjectionTest {
+
+	@Autowired
+	Knight knight;
+
+	@Autowired
+	FakePrintStream printStream;
+
+	@After
+	public void clearPrintStream() {
+		printStream.clear();
+	}
+
+	@Test
+	public void shouldInjectKnightWithSlayDragonQuest() {
+		knight.embarkOnQuest();
+		assertEquals("Embark on quest to slay the dragon!\n", printStream.getPrintedString());
+	}
+}
