99330aa0be74848443dd106008b4210c0ffa3e18
Winston Spencer Sonnesen <sonnesen@gmail.com>
2016-11-18 00:27:30 -0200

    Chapter 01

--- chapter01/knight/src/main/java/com/springinaction/knights/KnightMain.java
+++ chapter01/knight/src/main/java/com/springinaction/knights/KnightMain.java
@@ -6,6 +6,7 @@ public class KnightMain {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knights.xml");
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(KnightConfig.class);
 		Knight knight = context.getBean(Knight.class);
 		knight.embarkOnQuest();
 		context.close();
