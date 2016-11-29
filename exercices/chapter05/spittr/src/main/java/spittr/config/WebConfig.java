3fbebd5c3d0ae24f1f12ea4e1c999ccdb3de8991
Winston Spencer Sonnesen <sonnesen@gmail.com>
2016-11-28 23:13:58 -0200

    Chapter 05 - until section 5.2.2

--- exercices/chapter05/spittr/src/main/java/spittr/config/WebConfig.java
+++ exercices/chapter05/spittr/src/main/java/spittr/config/WebConfig.java
@@ -3,13 +3,31 @@ package spittr.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("spitter.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+	@Bean
+	public ViewResolver viewResolver() {
+		// Configure a JSP view resolver
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// configure static content handling
+		configurer.enable();
+	}
 	
 }
